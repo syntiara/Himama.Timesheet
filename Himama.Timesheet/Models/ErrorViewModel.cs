08e25476399b6cc1bf99ae81af7dd0f696fcf2b2
Cynthia Okoliezeh <cynthia.okoliezeh@gmail.com>
2021-07-15 02:06:45 -0400

    initial commit

---
+++ Himama.Timesheet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Himama.Timesheet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
